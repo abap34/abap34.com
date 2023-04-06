f369eb4829ab475b11e40f2e3ed907aa3bfbace9
Yuchi Yamaguchi <abap0002@gmail.com>
2023-04-07 01:07:36 +0900

    add python setup

--- blog_builder/build.py
+++ blog_builder/build.py
@@ -24,18 +24,15 @@ def build_article(raw_file, settings_file, output_file):
     ---
     '''
     
-    # 記事の本文をMarkdownからHTMLに変換する
     body = mistune.markdown(markdown)
     
-    # 記事のHTMLをファイルに書き込む
     with open(output_file, 'w') as f:
         f.write(header)
         f.write(body)
 
-# コマンドライン引数から記事の本文を保存するファイル名、記事のメタデータを保存するファイル名、記事のHTMLを保存するファイル名を取得する
-raw_file = sys.argv[1]
-settings_file = sys.argv[2]
-output_file = sys.argv[3]
+if __name__ == '__main__':
+    raw_file = sys.argv[1]
+    settings_file = sys.argv[2]
+    output_file = sys.argv[3]
 
-# 記事の本文とメタデータからHTMLを作成する
-build_article(raw_file, settings_file, output_file)
+    build_article(raw_file, settings_file, output_file)
