cb37b4c82146c0dd7dfbf8ed395dae82ae3b8ad9
abap34 <uchi_ymgc@her0.jp>
2023-11-07 02:42:07 +0900

    Create gen.sh

---
+++ posts/gen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+template="---
+title: 
+author: abap34
+date: $(date +'%Y/%m/%d')
+tag: 
+twitter_id: abap34
+github_id: abap34
+mail: abap0002@gmail.com
+ogp_url: 
+description: 
+url: https://abap34.com/posts/$(basename "$1" .md).html
+site_name: abap34.com
+twitter_site: @abap34
+---"
+
+echo "$template" > "$1"
+
+ogp_url=$(curl -s https://dog.ceo/api/breeds/image/random | jq -r '.message')
+sed -i -e "s|ogp_url: |ogp_url: $ogp_url|g" "$1"
+rm "$1-e"
+
+mkdir -p $(basename "$1" .md)
+
+echo "記事テンプレートが作成されました: $1"
