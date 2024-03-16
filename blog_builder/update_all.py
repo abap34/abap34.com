a25bfd362960f5157569321efde6587e975bad7b
abap34 <uchi_ymgc@her0.jp>
2024-03-17 02:39:14 +0900

    up

--- blog_builder/update_all.py
+++ blog_builder/update_all.py
@@ -1,25 +1,15 @@
-import sys
 import glob
 
 
-def main(rm=False):
+def main():
     for path in glob.glob('posts/*.md'):
         with open(path, 'r') as f:
-            if rm:
-                text = f.read().split('\n', 1)[0]
-            else:
-                text = f.read()
-
+            # 末尾にスペースを追加
+            text = f.read() + ' '
+            
         with open(path, 'w') as f:
-            if rm:
-                f.write(
-                    text.split('\n')[-1]
-                )
-
-            else:
-                f.write(text)
+            f.write(text)
 
         
 if __name__ == '__main__':
-    # コマンドライン引数で -d オプションが指定された場合は main(rm=True) を実行
-    main('-d' in sys.argv)
+    main()
