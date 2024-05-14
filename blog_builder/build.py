4f5c186de1c344c39722a7465d095b98e2a2fa8f
abap34 <abap0002@gmail.com>
2024-05-15 00:12:53 +0900

    Update build.py

--- blog_builder/build.py
+++ blog_builder/build.py
@@ -8,102 +8,95 @@ CONFIG_CORRESPONDING = {
     "custom_css": "-c",
     "editor_theme": "-e",
     "syntax_theme": "-s",
-    "template": "-b"
+    "template": "-b",
 }
 
+
 def load_json(path: pathlib.Path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return json.load(f)
 
 
-def load_rawtext(ir, result=''):
-    if 'childs' in ir.keys():
-        for child in ir['childs']:
+def load_rawtext(ir, result=""):
+    if "childs" in ir.keys():
+        for child in ir["childs"]:
             result += load_rawtext(child)
     else:
-        if 'content' in ir.keys():
-            result += ir['content']
-        elif ir['class'] == 'NewLine':
-            result += '\n'
+        if "content" in ir.keys():
+            result += ir["content"]
+        elif ir["class"] == "NewLine":
+            result += "\n"
 
     return result
 
 
 def to_outputpath(article_path: pathlib.Path):
-    return pathlib.Path(
-        'public/posts/'
-        + article_path.stem
-        + '.html'
-    )
+    return pathlib.Path("public/posts/" + article_path.stem + ".html")
 
 
 def build_article(config: dict, article_path: pathlib.Path):
     outputpath = to_outputpath(article_path)
 
-    subprocess.run(
-        'cp -r posts/{} public/posts/'.format(article_path.stem),
-        shell=True
-    )
+    subprocess.run("cp -r posts/{} public/posts/".format(article_path.stem), shell=True)
 
-    cmd = 'ALMO/build/almo {} -o {} -d'.format(article_path, outputpath)
+    cmd = "ALMO/build/almo {} -o {} -d".format(article_path, outputpath)
 
     for key, value in CONFIG_CORRESPONDING.items():
         if key in config:
-            cmd += ' {} {}'.format(value, config[key])
+            cmd += " {} {}".format(value, config[key])
 
-    cmd += '> tmp.json'
+    cmd += "> tmp.json"
 
-    print(
-        'Building article with the following command: \n'
-        + cmd
-    )
+    print("Building article with the following command: \n" + cmd)
 
     subprocess.run(cmd, shell=True)
 
-    with open('tmp.json', 'r') as f:
+    with open("tmp.json", "r") as f:
         tmp = json.load(f)
-        title = tmp['meta']['title']
-        date = tmp['meta']['date']
-        ogp_url = tmp['meta']['ogp_url']
-        ir = tmp['ir']
-        tags = tmp['meta']['tag']
+        title = tmp["meta"]["title"]
+        date = tmp["meta"]["date"]
+        ogp_url = tmp["meta"]["ogp_url"]
+        ir = tmp["ir"]
+        tags = tmp["meta"]["tag"]
 
-    url = config["root_url"] + '/posts/' + outputpath.name
+    url = config["root_url"] + "/posts/" + outputpath.name
 
     content = load_rawtext(ir)
 
-    tags = tags[1:-1].split(',')
+    tags = tags[1:-1].split(",")
     tags = [tag.strip() for tag in tags]
 
-    with open('public/posts.json', 'r') as f:
+    with open("public/posts.json", "r") as f:
         posts = json.load(f)
         for post in posts:
-            if post['url'] == url:
+            if post["url"] == url:
                 updated_post = {
-                    'title': title,
-                    'post_date': date,
-                    'url': url,
-                    'thumbnail_url': ogp_url,
-                    'content': content,
-                    'tags': tags
+                    "title": title,
+                    "post_date": date,
+                    "url": url,
+                    "thumbnail_url": ogp_url,
+                    "content": content,
+                    "tags": tags,
                 }
                 posts.remove(post)
                 posts.append(updated_post)
 
                 break
         else:
-            posts.append({
-                'title': title,
-                'post_date': date,
-                'url': url,
-                'thumbnail_url': ogp_url,
-                'content': content,
-                'tags': tags
-            }) 
+            posts.append(
+                {
+                    "title": title,
+                    "post_date": date,
+                    "url": url,
+                    "thumbnail_url": ogp_url,
+                    "content": content,
+                    "tags": tags,
+                }
+            )
 
-    posts = sorted(posts, key=lambda x: x['post_date'], reverse=True)
+    posts = sorted(posts, key=lambda x: x["post_date"], reverse=True)
 
-    with open('public/posts.json', 'w') as f:
+    with open("public/posts.json", "w") as f:
         json.dump(posts, f)
 
 
@@ -112,21 +105,19 @@ def build(config: dict, article_paths: list[pathlib.Path]):
         build_article(config, article_path)
 
 
-if __name__ == '__main__':
-    if os.getenv('REBUILD'):
-        with open('public/posts.json', 'w') as f:
-            f.write('[]')
-        with open('public/recent_posts.json', 'w') as f:
-            f.write('[]')
+if __name__ == "__main__":
+    if os.getenv("REBUILD"):
+        with open("public/posts.json", "w") as f:
+            f.write("[]")
+        with open("public/recent_posts.json", "w") as f:
+            f.write("[]")
 
-        config = load_json(pathlib.Path('config/config.json'))
-        build(config, list(pathlib.Path('posts').glob('*.md')))
-        
+        config = load_json(pathlib.Path("config/config.json"))
+        build(config, list(pathlib.Path("posts").glob("*.md")))
+
     else:
-        change = load_json(pathlib.Path('changed_files.json'))
-        config = load_json(pathlib.Path('config/config.json'))
+        change = load_json(pathlib.Path("changed_files.json"))
+        config = load_json(pathlib.Path("config/config.json"))
 
         for article_path in change:
             build_article(config, pathlib.Path(article_path))
-
-    
