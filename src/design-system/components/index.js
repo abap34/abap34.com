6f2ee6ba3e351603cda1a855f586f6ca775ab17c
abap34 <abap0002@gmail.com>
2025-08-15 01:55:53 +0900

    wip

--- src/design-system/components/index.js
+++
@@ -1,7 +0,0 @@
-export { Button } from './Button';
-export { Badge } from './Badge';
-export { Switch } from './Switch';
-export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './Card';
-export { Heading, Text, Link } from './Typography';
-export { Section } from './Section';
-export { Tag } from './Tag';
